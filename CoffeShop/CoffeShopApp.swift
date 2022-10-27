c82ef02db06daba89f555dec7d8acd1d3efa7ebf
Joseph Murillo <josephmurillo2425@gmail.com>
2022-10-27 16:38:22 -0600

    Initial Commit

---
+++ CoffeShop/CoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopApp.swift
+//  CoffeShop
+//
+//  Created by Joseph Murillo on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
